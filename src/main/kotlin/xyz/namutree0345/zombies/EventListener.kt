82c986e6ef7eeed47487f257d4842d54a53b194a
namutree0345 <johanbag290@gmail.com>
2021-04-10 21:06:27 +0900

    Add: Zombie player health bar is green

--- src/main/kotlin/xyz/namutree0345/zombies/EventListener.kt
+++ src/main/kotlin/xyz/namutree0345/zombies/EventListener.kt
@@ -1,8 +1,12 @@
 package xyz.namutree0345.zombies
 
+import org.bukkit.Bukkit
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerJoinEvent
+import org.bukkit.plugin.java.JavaPlugin
+import org.bukkit.potion.PotionEffect
+import org.bukkit.potion.PotionEffectType
 
 class EventListener : Listener {
 
@@ -10,6 +14,17 @@ class EventListener : Listener {
     fun join(event: PlayerJoinEvent) {
         event.player.scoreboard = board!!
         zombieTeam?.addEntry(event.player.name)
+
+        val taskId = Bukkit.getScheduler().scheduleSyncRepeatingTask(JavaPlugin.getPlugin(Zombies::class.java), {
+            if(humanTeam?.hasEntry(event.player.name) == false) {
+                event.player.addPotionEffect(PotionEffect(PotionEffectType.POISON, 5, 0, false, false))
+            }
+        }, 0, 1)
+        Bukkit.getScheduler().scheduleSyncRepeatingTask(JavaPlugin.getPlugin(Zombies::class.java), {
+            if(!Bukkit.getOnlinePlayers().contains(event.player)) {
+                Bukkit.getScheduler().cancelTask(taskId)
+            }
+        }, 0, 1)
     }
 
 }
