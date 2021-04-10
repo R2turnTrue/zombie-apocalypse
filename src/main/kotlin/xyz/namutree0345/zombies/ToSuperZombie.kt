61800e837bc021e8fbb15d73dd39321aba7b45bf
namutree0345 <johanbag290@gmail.com>
2021-04-10 18:17:57 +0900

    Add superzombieise

---
+++ src/main/kotlin/xyz/namutree0345/zombies/ToSuperZombie.kt
@@ -0,0 +1,28 @@
+package xyz.namutree0345.zombies
+
+import org.bukkit.Bukkit
+import org.bukkit.ChatColor
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.entity.PlayerDeathEvent
+
+class ToSuperZombie : Listener {
+
+    @EventHandler
+    fun died(event: PlayerDeathEvent) {
+        for (player in Bukkit.getOnlinePlayers()) {
+            if(humanTeam?.hasEntry(event.entity.name) == true) {
+                humanTeam?.removeEntry(event.entity.name)
+                superZombieTeam?.addEntry(event.entity.name)
+                player.sendTitle(
+                    "${ChatColor.RED}생존자 사망",
+                    "${ChatColor.RED}${event.entity.name}님이 슈퍼 좀비가 되었습니다!",
+                    10,
+                    70,
+                    20
+                )
+            }
+        }
+    }
+
+}
