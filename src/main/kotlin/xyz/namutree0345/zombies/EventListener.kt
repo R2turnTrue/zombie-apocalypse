7e0bcf4e2dae88b15b21e00944a9107a41a8535b
namutree0345 <johanbag290@gmail.com>
2021-04-11 14:44:27 +0900

    .

--- src/main/kotlin/xyz/namutree0345/zombies/EventListener.kt
+++ src/main/kotlin/xyz/namutree0345/zombies/EventListener.kt
@@ -1,8 +1,10 @@
 package xyz.namutree0345.zombies
 
 import org.bukkit.Bukkit
+import org.bukkit.entity.*
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.entity.EntityTargetEvent
 import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.plugin.java.JavaPlugin
 import org.bukkit.potion.PotionEffect
@@ -18,6 +20,7 @@ class EventListener : Listener {
         val taskId = Bukkit.getScheduler().scheduleSyncRepeatingTask(JavaPlugin.getPlugin(Zombies::class.java), {
             if(humanTeam?.hasEntry(event.player.name) == false) {
                 event.player.addPotionEffect(PotionEffect(PotionEffectType.POISON, 5, 0, false, false))
+                event.player.addPotionEffect(PotionEffect(PotionEffectType.NIGHT_VISION, 5, 255, false, false))
             }
         }, 0, 1)
         Bukkit.getScheduler().scheduleSyncRepeatingTask(JavaPlugin.getPlugin(Zombies::class.java), {
@@ -25,6 +28,17 @@ class EventListener : Listener {
                 Bukkit.getScheduler().cancelTask(taskId)
             }
         }, 0, 1)
+    }
+
+    @EventHandler
+    fun mobTarget(event: EntityTargetEvent) {
+        if(event.entity is Monster) {
+            if(event.target is Player) {
+                if(zombieTeam?.hasEntry(event.target?.name!!) == true || superZombieTeam?.hasEntry(event.target?.name!!) == true) {
+                    event.isCancelled = true
+                }
+            }
+        }
     }
 
 }
