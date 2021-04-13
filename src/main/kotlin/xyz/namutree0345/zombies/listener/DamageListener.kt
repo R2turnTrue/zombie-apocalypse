0a2bbed442cf0749b186290c24c781fb226a6d39
namutree0345 <johanbag290@gmail.com>
2021-04-13 12:26:53 +0900

    Add parming time

--- src/main/kotlin/xyz/namutree0345/zombies/listener/DamageListener.kt
+++ src/main/kotlin/xyz/namutree0345/zombies/listener/DamageListener.kt
@@ -8,12 +8,19 @@ import org.bukkit.event.entity.EntityDamageByEntityEvent
 import org.bukkit.event.entity.EntityDamageEvent
 import org.bukkit.potion.PotionEffect
 import org.bukkit.potion.PotionEffectType
+import xyz.namutree0345.zombies.command.mujuk
 import xyz.namutree0345.zombies.superZombieTeam
 import xyz.namutree0345.zombies.zombieTeam
 
 class DamageListener : Listener {
     @EventHandler
     fun damage(event: EntityDamageEvent) {
+
+        if(mujuk) {
+            event.isCancelled = true
+            return
+        }
+
         if(event.entityType == EntityType.PLAYER && event.cause == EntityDamageEvent.DamageCause.FALL) {
             event.damage = 0.0
             (event.entity as Player).addPotionEffect(PotionEffect(PotionEffectType.SLOW, 40, 5, false, false))
@@ -22,6 +29,12 @@ class DamageListener : Listener {
 
     @EventHandler
     fun damageByZombie(event: EntityDamageByEntityEvent) {
+
+        if(mujuk) {
+            event.isCancelled = true
+            return
+        }
+
         if(event.damager is Player && event.entity is Player) {
             if(zombieTeam?.hasEntry(event.damager.name) == true || superZombieTeam?.hasEntry(event.damager.name) == true) {
                 if(zombieTeam?.hasEntry(event.entity.name) == true || superZombieTeam?.hasEntry(event.entity.name) == true) {
