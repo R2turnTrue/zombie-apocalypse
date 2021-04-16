9e0ce5f598cd47228e178f2db02ef6673ae5583e
namutree0345 <johanbag290@gmail.com>
2021-04-16 22:41:00 +0900

    ttt

--- src/main/kotlin/xyz/namutree0345/zombies/listener/RemovePlayerFromPlayerList.kt
+++ src/main/kotlin/xyz/namutree0345/zombies/listener/RemovePlayerFromPlayerList.kt
@@ -1,6 +1,8 @@
 package xyz.namutree0345.zombies.listener
 
 import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.NamedTextColor
+import net.kyori.adventure.text.format.TextDecoration
 import net.minecraft.server.v1_16_R3.PacketPlayOutPlayerInfo
 import org.bukkit.Bukkit
 import org.bukkit.craftbukkit.v1_16_R3.entity.CraftPlayer
@@ -23,12 +25,12 @@ class RemovePlayerFromPlayerList : Listener {
     @EventHandler
     fun join(event: PlayerJoinEvent) {
         event.joinMessage(Component.empty())
+        event.player.playerListName(Component.text("abcdefgh", NamedTextColor.WHITE, TextDecoration.OBFUSCATED))
 
         for(p in Bukkit.getOnlinePlayers()) {
             val pc = p as CraftPlayer
-            val packet = PacketPlayOutPlayerInfo(PacketPlayOutPlayerInfo.EnumPlayerInfoAction.REMOVE_PLAYER, pc.handle)
 
-            (event.player as CraftPlayer).handle.playerConnection.sendPacket(packet)
+            (event.player as CraftPlayer).handle.playerConnection.sendPacket(PacketPlayOutPlayerInfo(PacketPlayOutPlayerInfo.EnumPlayerInfoAction.REMOVE_PLAYER, pc.handle))
         }
     }
 
